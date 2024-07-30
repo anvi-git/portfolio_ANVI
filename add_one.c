af6784526fccbe5e10a009287423e287ea27e58e
Antonio Viscusi <139615741+anvi-git@users.noreply.github.com>
2024-07-30 18:12:07 +0200

    #1 - some simple program in C

---
+++ add_one.c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int successivo(int a);
+
+int main()
+  {
+    using namespace std;
+    int a, b;
+  
+    a=2;
+    b=a+1;
+    return 0;
+  }
+ int successivo(int x);
+  {
+  return x+1;
+  }
