468c4bb891c5ea96d04dcf6ae4c1a34963abeb6e
Antonio Viscusi <139615741+anvi-git@users.noreply.github.com>
2024-08-01 17:34:25 +0200

    Simple program to count numbers

---
+++ simple_program_to_count_numbers.cpp
@@ -0,0 +1,18 @@
+// this is a very simple program that counts and prints out the number
+
+#include <iostream> 
+#include <cmath>
+	 
+int main()
+{
+ using namespace std;
+ int i;
+
+  for(int i=1;i<=10;i++)
+  {
+    	cout << "Numero: " << i << endl;
+  }
+  return 0;
+ }
+
+
